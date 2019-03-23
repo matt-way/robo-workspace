35e86daa06633a7fd126b2ac07c784f579898dc8
Matt Way <matt@syntropy.xyz>
2019-03-23 20:42:46 +1000

    port to new version

--- programs/printer-discrete-arm/blocks/arm-renderer/code.js
+++ programs/printer-discrete-arm/blocks/arm-renderer/code.js
@@ -1,44 +1,42 @@
-import { random } from "lodash"
+import { random } from 'lodash'
 
 const negFixed = (val, dec) => {
   const res = val.toFixed(dec)
   return res.indexOf('-') >= 0 ? res : ` ${res}`
 }
 
-export const run = ({ state, output }) => {
-  state.canvas = document.createElement("canvas")
+export const run = ({ state, element }) => {
+  state.canvas = document.createElement('canvas')
   state.canvas.style.border = '1px solid black'
   state.canvas.style.margin = '10px'
-  state.ctx = state.canvas.getContext("2d")
+  state.ctx = state.canvas.getContext('2d')
   state.canvas.width = 300
   state.canvas.height = 300
-  output.appendChild(state.canvas)
+  element.appendChild(state.canvas)
 }
 
 export const update = ({ state, iteration }) => {
-  const { 
-    canvas, ctx, position, motion, history
-  } = state
+  const { canvas, ctx, position, motion, history } = state
   const size = 4
   const half = Math.floor(size / 2)
-  
+
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  
+
   ctx.fillStyle = 'rgba(0,0,0,0.1)'
   history.forEach(([x, y]) => {
     const sx = Math.floor(0.5 * canvas.width)
-  	const sy = Math.floor(0.7 * canvas.height)    
-	  const elen = 70
- 		const ex = elen * Math.cos(x * 180 * Math.PI / 180)
-  	const ey = elen * Math.sin(x * 180 * Math.PI / 180)
-		const flen = 70  
-  	const fx = flen * Math.cos((x + y) * 180 * Math.PI / 180)
-  	const fy = flen * Math.sin((x + y) * 180 * Math.PI / 180)
+    const sy = Math.floor(0.7 * canvas.height)
+    const elen = 70
+    const ex = elen * Math.cos((x * 180 * Math.PI) / 180)
+    const ey = elen * Math.sin((x * 180 * Math.PI) / 180)
+    const flen = 70
+    const fx = flen * Math.cos(((x + y) * 180 * Math.PI) / 180)
+    const fy = flen * Math.sin(((x + y) * 180 * Math.PI) / 180)
     const xx = sx + ex + fx
     const yy = sy - ey - fy
-  	ctx.fillRect(xx - half, yy - half, size, size)
+    ctx.fillRect(xx - half, yy - half, size, size)
   })
-  
+
   //ctx.fillStyle = 'red'
   //ctx.fillRect(position[0] * canvas.width - half, position[1] * canvas.height - half, size, size)
   ctx.strokeStyle = 'red'
@@ -46,7 +44,7 @@ export const update = ({ state, iteration }) => {
   const sx = Math.floor(0.5 * canvas.width)
   const sy = Math.floor(0.7 * canvas.height)
   ctx.moveTo(sx, sy)
-  
+
   /*const aa = 0.5
   const bb = 0.5
   
@@ -59,22 +57,31 @@ export const update = ({ state, iteration }) => {
   const fx = flen * Math.cos((aa + bb) * 180 * Math.PI / 180)
   const fy = flen * Math.sin((aa + bb) * 180 * Math.PI / 180)
   */
-    
+
   const elen = 70
-  const ex = elen * Math.cos(position[0] * 180 * Math.PI / 180)
-  const ey = elen * Math.sin(position[0] * 180 * Math.PI / 180)
+  const ex = elen * Math.cos((position[0] * 180 * Math.PI) / 180)
+  const ey = elen * Math.sin((position[0] * 180 * Math.PI) / 180)
   ctx.lineTo(sx + ex, sy - ey)
-  
-	const flen = 70  
-  const fx = flen * Math.cos((position[0] + position[1]) * 180 * Math.PI / 180)
-  const fy = flen * Math.sin((position[0] + position[1]) * 180 * Math.PI / 180)
-  
+
+  const flen = 70
+  const fx =
+    flen * Math.cos(((position[0] + position[1]) * 180 * Math.PI) / 180)
+  const fy =
+    flen * Math.sin(((position[0] + position[1]) * 180 * Math.PI) / 180)
+
   ctx.lineTo(sx + ex + fx, sy - ey - fy)
   ctx.stroke()
-  
-  ctx.fillStyle = 'rgba(0,0,0,0.5)'  
-  ctx.fillText(`x: ${position[0].toFixed(2)} y: ${position[1].toFixed(2)}`, 6, 15)
-  ctx.fillText(`dx: ${negFixed(motion[0], 2)} dy: ${negFixed(motion[1], 2)}`, 6, 30)
+
+  ctx.fillStyle = 'rgba(0,0,0,0.5)'
+  ctx.fillText(
+    `x: ${position[0].toFixed(2)} y: ${position[1].toFixed(2)}`,
+    6,
+    15
+  )
+  ctx.fillText(
+    `dx: ${negFixed(motion[0], 2)} dy: ${negFixed(motion[1], 2)}`,
+    6,
+    30
+  )
   ctx.fillText(`iteration: ${iteration}`, 6, 45)
 }
-
--- programs/printer-discrete-arm/blocks/graph-renderer-2d/code.js
+++ programs/printer-discrete-arm/blocks/graph-renderer-2d/code.js
@@ -1,25 +1,25 @@
-export const run = ({ state, output }) => {
-	state.canvas = document.createElement('canvas')
+export const run = ({ state, element }) => {
+  state.canvas = document.createElement('canvas')
   state.canvas.style.border = '1px solid black'
   state.canvas.style.margin = '10px'
-  state.ctx = state.canvas.getContext("2d")
+  state.ctx = state.canvas.getContext('2d')
   state.canvas.width = 300
   state.canvas.height = 300
-  output.appendChild(state.canvas)
+  element.appendChild(state.canvas)
 }
 
 export const update = ({ state }) => {
   const { canvas, ctx, graph } = state
   const size = 4
   const half = Math.floor(size / 2)
-  
+
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  
-  graph.forEachNode(function(node){
+
+  graph.forEachNode(function(node) {
     const feature = node.data.feature.data
     const sx = feature[0] * canvas.width
     const sy = feature[1] * canvas.height
-    
+
     // draw the links
     /*var avgIncidence = 0  
     graph.forEachLinkedNode(node.id, function(linkedNode, link){
@@ -27,64 +27,63 @@ export const update = ({ state }) => {
     })
     avgIncidence /= node.links.length
     */
-    
+
     ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-    graph.forEachLinkedNode(node.id, function(linkedNode, link){
-      // only draw useful links      
+    graph.forEachLinkedNode(node.id, function(linkedNode, link) {
+      // only draw useful links
       //if(link.data.incidence > 0.1 * avgIncidence){
-      	const tFeature = linkedNode.data.feature.data
-				const tx = tFeature[0] * canvas.width
-      	const ty = tFeature[1] * canvas.height
-	      
-	      ctx.beginPath()
-				ctx.moveTo(sx, sy)
-				ctx.lineTo(tx, ty)
-				ctx.stroke()
+      const tFeature = linkedNode.data.feature.data
+      const tx = tFeature[0] * canvas.width
+      const ty = tFeature[1] * canvas.height
+
+      ctx.beginPath()
+      ctx.moveTo(sx, sy)
+      ctx.lineTo(tx, ty)
+      ctx.stroke()
       //}
-		})    
-    
-    if(state.globalTarget === node.id){
+    })
+
+    if (state.globalTarget === node.id) {
       ctx.fillStyle = 'blue'
-    }else if(state.lastWinner === node.id){
+    } else if (state.lastWinner === node.id) {
       ctx.fillStyle = 'red'
-    }else{
-    	ctx.fillStyle = 'green'  
-    }    
-	  ctx.fillRect(sx - half, sy - half, size, size)
-                
+    } else {
+      ctx.fillStyle = 'green'
+    }
+    ctx.fillRect(sx - half, sy - half, size, size)
+
     ctx.fillStyle = 'blue'
     const { position, randomTargets, randomChoice } = state
     const px = position[0]
     const py = position[1]
     const pxc = px * canvas.width
     const pyc = py * canvas.height
-    ctx.fillRect(pxc - half,pyc - half, size, size)
-    
-    if(state.mode === 'explore'){
-      
-      for(var v=0; v<randomTargets.length; v++){
+    ctx.fillRect(pxc - half, pyc - half, size, size)
+
+    if (state.mode === 'explore') {
+      for (var v = 0; v < randomTargets.length; v++) {
         const t = randomTargets[v].data
         const vx = t[0] * 10
         const vy = t[1] * 10
 
-        ctx.strokeStyle = 'grey'    
+        ctx.strokeStyle = 'grey'
         ctx.beginPath()
         ctx.moveTo(pxc, pyc)
         ctx.lineTo((px + vx) * canvas.width, (py + vy) * canvas.height)
-        ctx.stroke()    
+        ctx.stroke()
       }
 
       const t = randomChoice.data
-      if(t){
-      	const vx = t[0] * 10
-      	const vy = t[1] * 10
+      if (t) {
+        const vx = t[0] * 10
+        const vy = t[1] * 10
 
-      	ctx.strokeStyle = 'pink'    
-      	ctx.beginPath()
-      	ctx.moveTo(pxc, pyc)
-      	ctx.lineTo((px + vx) * canvas.width, (py + vy) * canvas.height)
-      	ctx.stroke()                    
+        ctx.strokeStyle = 'pink'
+        ctx.beginPath()
+        ctx.moveTo(pxc, pyc)
+        ctx.lineTo((px + vx) * canvas.width, (py + vy) * canvas.height)
+        ctx.stroke()
       }
     }
-	})
+  })
 }
